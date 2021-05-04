11b051ec10e16c23f95a25369df0389947ee4914
PiotrMrozek <phineas122@gmail.com>
2021-05-04 22:46:45 +0200

    Zadanie 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -27,5 +27,19 @@ public class Main {
             me.pet.feed();
 
 
+            //Zadanie 2
+
+            Car passat = new Car("Passat", "vw");
+            passat.color = "red";
+            passat.millage = 100000.0;
+            passat.engineVolume = 1.9;
+
+            System.out.println("takie auto: " + passat.producer + " "+ passat.model);
+
+            me.car = passat;
+            System.out.println("Mam auto: " + me.car.model);
+
+
+
     }
 }
